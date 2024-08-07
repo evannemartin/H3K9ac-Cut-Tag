2d1ba709ce074542809983a12e713eac60e56be8
evannemartin <evanne.martin@insa-lyon.fr>
2024-08-07 06:52:16 +0200

    Separate permutation test in T1 and T2 LADs

--- tau_lad_intersect_analysis.R
+++ tau_lad_intersect_analysis.R
@@ -13,26 +13,39 @@ setwd(projPath)
 ############################ Permutation test #################################
 
 # Import
-LAD_peaks <- import("../LAD domains/sorted_GSM5669198_D5Midbrain_LADs_mm39.bed",
-                    format = "BED")
-enriched_KD_peaks <- import("differential_analysis/enriched_KD_masterPeak_sep.ucsc.bed",
-                            format = "BED")
-lost_KD_peaks <- import("differential_analysis/lost_KD_masterPeak_sep.ucsc.bed",
-                        format = "BED")
-enriched_OE_peaks <- import("differential_analysis/enriched_OE_masterPeak_sep.ucsc.bed",
-                            format = "BED")
-lost_OE_peaks <- import("differential_analysis/lost_OE_masterPeak_sep.ucsc.bed",
-                        format = "BED")
+T1_LAD_peaks <- import("../LAD domains/T1_GSM5669198_D5Midbrain_LADs_mm39.bed",
+                       format = "BED")
+T2_LAD_peaks <- import("../LAD domains/T2_GSM5669198_D5Midbrain_LADs_mm39.bed",
+                       format = "BED")
+enriched_KD_peaks <- import(
+  "differential_analysis/enriched_KD_masterPeak_sep_without_outliers_padj0.1.ucsc.bed",
+  format = "BED"
+)
+lost_KD_peaks <- import(
+  "differential_analysis/lost_KD_masterPeak_sep_without_outliers_padj0.1.ucsc.bed",
+  format = "BED"
+)
+enriched_OE_peaks <- import(
+  "differential_analysis/enriched_OE_masterPeak_sep_without_outliers_padj0.1.ucsc.bed",
+  format = "BED"
+)
+lost_OE_peaks <- import(
+  "differential_analysis/lost_OE_masterPeak_sep_without_outliers_padj0.1.ucsc.bed",
+  format = "BED"
+)
 
-len_LAD_peaks <- length(LAD_peaks)
+len_T1_LAD_peaks <- length(T1_LAD_peaks)
+len_T2_LAD_peaks <- length(T2_LAD_peaks)
 len_enriched_KD_peaks <- length(enriched_KD_peaks)
 len_lost_KD_peaks <- length(lost_KD_peaks)
 len_enriched_OE_peaks <- length(enriched_OE_peaks)
 len_lost_OE_peaks <- length(lost_OE_peaks)
 
 # Filter to have only canonical
-LAD_peaks <- filterChromosomes(LAD_peaks, organism = "mm39", chr.type =
-                                 "canonical")
+T1_LAD_peaks <- filterChromosomes(T1_LAD_peaks, organism = "mm39", chr.type =
+                                    "canonical")
+T2_LAD_peaks <- filterChromosomes(T2_LAD_peaks, organism = "mm39", chr.type =
+                                    "canonical")
 enriched_KD_peaks <- filterChromosomes(enriched_KD_peaks, organism = "mm39", chr.type =
                                          "canonical")
 lost_KD_peaks <- filterChromosomes(lost_KD_peaks, organism = "mm39", chr.type =
@@ -42,10 +55,11 @@ enriched_OE_peaks <- filterChromosomes(enriched_OE_peaks, organism = "mm39", chr
 lost_OE_peaks <- filterChromosomes(lost_OE_peaks, organism = "mm39", chr.type =
                                      "canonical")
 
-# Permutation test
+### Permutation test with T1
+
 enriched_KD_analysis <- overlapPermTest(
   A = enriched_KD_peaks,
-  B = LAD_peaks,
+  B = T1_LAD_peaks,
   ntimes = 500,
   genome = "mm39",
   count.once = TRUE,
@@ -56,6 +70,8 @@ plot(enriched_KD_analysis)
 862/len_enriched_KD_peaks
 
 pvalue_enriched_KD <- enriched_KD_analysis$numOverlaps$pval
+
+# Compute Odds Ratio
 
 random_overlap_nb_enriched_KD <- mean(enriched_KD_analysis$numOverlaps$permuted)
 observed_overlap_nb_enriched_KD <-  enriched_KD_analysis$numOverlaps$observed
@@ -69,7 +85,7 @@ log(OR_enriched_KD)
 
 lost_KD_analysis <- overlapPermTest(
   A = lost_KD_peaks,
-  B = LAD_peaks,
+  B = T1_LAD_peaks,
   ntimes = 500,
   genome = "mm39",
   count.once = TRUE,
@@ -93,7 +109,7 @@ log(OR_lost_KD)
 
 enriched_OE_analysis <- overlapPermTest(
   A = enriched_OE_peaks,
-  B = LAD_peaks,
+  B = T1_LAD_peaks,
   ntimes = 500,
   genome = "mm39",
   count.once = TRUE,
@@ -117,7 +133,7 @@ log(OR_enriched_OE)
 
 lost_OE_analysis <- overlapPermTest(
   A = lost_OE_peaks,
-  B = LAD_peaks,
+  B = T1_LAD_peaks,
   ntimes = 500,
   genome = "mm39",
   count.once = TRUE,
@@ -137,3 +153,101 @@ observed_nonoverlap_nb_lost_OE <- len_lost_OE_peaks-observed_overlap_nb_lost_OE
 OR_lost_OE = (observed_overlap_nb_lost_OE / random_overlap_nb_lost_OE)/(observed_nonoverlap_nb_lost_OE /
                                                                           random_nonoverlap_nb_lost_OE)
 log(OR_lost_OE)
+
+
+### Permutation test with T2
+
+enriched_KD_analysis_T2 <- overlapPermTest(
+  A = enriched_KD_peaks,
+  B = T2_LAD_peaks,
+  ntimes = 500,
+  genome = "mm39",
+  count.once = TRUE,
+  mask = NA
+)
+enriched_KD_analysis_T2
+plot(enriched_KD_analysis_T2)
+# 862/len_enriched_KD_peaks
+#
+# pvalue_enriched_KD <- enriched_KD_analysis$numOverlaps$pval
+#
+# random_overlap_nb_enriched_KD <- mean(enriched_KD_analysis$numOverlaps$permuted)
+# observed_overlap_nb_enriched_KD <-  enriched_KD_analysis$numOverlaps$observed
+# random_nonoverlap_nb_enriched_KD <- len_enriched_KD_peaks-random_overlap_nb_enriched_KD
+# observed_nonoverlap_nb_enriched_KD <- len_enriched_KD_peaks-observed_overlap_nb_enriched_KD
+#
+# OR_enriched_KD = (observed_overlap_nb_enriched_KD / random_overlap_nb_enriched_KD)/(observed_nonoverlap_nb_enriched_KD /
+#                                                                                       random_nonoverlap_nb_enriched_KD)
+# log(OR_enriched_KD)
+
+
+lost_KD_analysis_T2 <- overlapPermTest(
+  A = lost_KD_peaks,
+  B = T2_LAD_peaks,
+  ntimes = 500,
+  genome = "mm39",
+  count.once = TRUE,
+  mask = NA
+)
+lost_KD_analysis_T2
+plot(lost_KD_analysis_T2)
+# 1082/len_lost_KD_peaks
+#
+# pvalue_lost_KD <- lost_KD_analysis$numOverlaps$pval
+#
+# random_overlap_nb_lost_KD <- mean(lost_KD_analysis$numOverlaps$permuted)
+# observed_overlap_nb_lost_KD <-  lost_KD_analysis$numOverlaps$observed
+# random_nonoverlap_nb_lost_KD <- len_lost_KD_peaks-random_overlap_nb_lost_KD
+# observed_nonoverlap_nb_lost_KD <- len_lost_KD_peaks-observed_overlap_nb_lost_KD
+#
+# OR_lost_KD = (observed_overlap_nb_lost_KD / random_overlap_nb_lost_KD)/(observed_nonoverlap_nb_lost_KD /
+#                                                                           random_nonoverlap_nb_lost_KD)
+# log(OR_lost_KD)
+
+
+enriched_OE_analysis_T2 <- overlapPermTest(
+  A = enriched_OE_peaks,
+  B = T2_LAD_peaks,
+  ntimes = 500,
+  genome = "mm39",
+  count.once = TRUE,
+  mask = NA
+)
+enriched_OE_analysis_T2
+plot(enriched_OE_analysis_T2)
+# 472/len_enriched_OE_peaks
+#
+# pvalue_enriched_OE <- enriched_OE_analysis$numOverlaps$pval
+#
+# random_overlap_nb_enriched_OE <- mean(enriched_OE_analysis$numOverlaps$permuted)
+# observed_overlap_nb_enriched_OE <-  enriched_OE_analysis$numOverlaps$observed
+# random_nonoverlap_nb_enriched_OE <- len_enriched_OE_peaks-random_overlap_nb_enriched_OE
+# observed_nonoverlap_nb_enriched_OE <- len_enriched_OE_peaks-observed_overlap_nb_enriched_OE
+#
+# OR_enriched_OE = (observed_overlap_nb_enriched_OE / random_overlap_nb_enriched_OE)/(observed_nonoverlap_nb_enriched_OE /
+#                                                                                       random_nonoverlap_nb_enriched_OE)
+# log(OR_enriched_OE)
+
+
+lost_OE_analysis_T2 <- overlapPermTest(
+  A = lost_OE_peaks,
+  B = T2_LAD_peaks,
+  ntimes = 500,
+  genome = "mm39",
+  count.once = TRUE,
+  mask = NA
+)
+lost_OE_analysis_T2
+plot(lost_OE_analysis_T2)
+# 933/len_lost_OE_peaks
+#
+# pvalue_lost_OE <- lost_OE_analysis$numOverlaps$pval
+#
+# random_overlap_nb_lost_OE <- mean(lost_OE_analysis$numOverlaps$permuted)
+# observed_overlap_nb_lost_OE <-  lost_OE_analysis$numOverlaps$observed
+# random_nonoverlap_nb_lost_OE <- len_lost_OE_peaks-random_overlap_nb_lost_OE
+# observed_nonoverlap_nb_lost_OE <- len_lost_OE_peaks-observed_overlap_nb_lost_OE
+#
+# OR_lost_OE = (observed_overlap_nb_lost_OE / random_overlap_nb_lost_OE)/(observed_nonoverlap_nb_lost_OE /
+#                                                                           random_nonoverlap_nb_lost_OE)
+# log(OR_lost_OE)
